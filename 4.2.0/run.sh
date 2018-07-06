23e7d80c9dee203d19b0266d8e2fca466ece3fcf
coder4 <lihy@fenbi.com>
2018-07-06 15:55:28 +0800

    FIX: it

--- 4.2.0/run.sh
+++ 4.2.0/run.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
 # pwd and env 
-cd bin && export JAVA_OPT=" -Duser.home=/opt"
+export JAVA_OPT=" -Duser.home=/opt"
 
-function launch_brocker_server() {
+function launch_broker_server() {
 
   if [ x"$NAME_SERVER_LIST" == x"" ];then
     echo "env var NAME_SERVER_LIST invalid"
     exit 1
   fi
 
-  sh mqbroker -n $NAME_SERVER_LIST 
+  if [ x"$1" == x"" ];then
+    echo "flag invalid"
+    exit 1
+  fi
+
+  sh ./bin/mqbroker -c ./conf/2m-noslave/broker-${1}.properties -n $NAME_SERVER_LIST
 
 }
 
 function launch_name_server() {
 
-  sh mqnamesrv 
+  sh ./bin/mqnamesrv
 
 }
 
-if [[ "${BROKER_SERVER}" == "true" ]]; then
-  launch_brocker_server
+if [[ "${BROKER_SERVER_A}" == "true" ]]; then
+  launch_broker_server "a"
+  exit 0
+fi
+
+if [[ "${BROKER_SERVER_B}" == "true" ]]; then
+  launch_broker_server "b"
   exit 0
 fi
 
