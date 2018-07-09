d494191aadb628cfad7b79769eadc90aa80a7002
coder4 <lihy@fenbi.com>
2018-07-09 18:57:16 +0800

    FIX: Support hehe

--- 4.2.0/run.sh
+++ 4.2.0/run.sh
@@ -3,7 +3,7 @@
 # pwd and env 
 export JAVA_OPT=" -Duser.home=/opt"
 
-function launch_broker_server() {
+function launch_broker_cluster_server() {
 
   if [ x"$NAME_SERVER_LIST" == x"" ];then
     echo "env var NAME_SERVER_LIST invalid"
@@ -11,11 +11,11 @@ function launch_broker_server() {
   fi
 
   if [ x"$1" == x"" ];then
-    echo "flag invalid"
+    echo "config invalid"
     exit 1
   fi
 
-  sh ./bin/mqbroker -c ./conf/2m-noslave/broker-${1}.properties -n $NAME_SERVER_LIST
+  sh ./bin/mqbroker -c $1 -n $NAME_SERVER_LIST
 
 }
 
@@ -25,19 +25,77 @@ function launch_name_server() {
 
 }
 
-if [[ "${BROKER_SERVER}" == "true" ]]; then
-  if [[ x"${BROKER_IDX}" == "0" ]]; then
-    launch_broker_server "a"
-  elif [[ x"${BROKER_IDX}" == "1" ]]; then
-    launch_broker_server "b"
-  else
-    echo "invalid BROCKER_IDX"
-    exit 1
-  fi
-fi
+function get_broker_cluster_id() {
 
-if [[ "${NAME_SERVER}" == "true" ]]; then
+  conf=$1
+  idx=$2
+
+  if [[ x"$conf" == x"2m-noslave" ]]; then
+    case $idx in
+      0)
+        echo "a"
+        ;;
+      1)
+        echo "b"
+        ;;
+      *)
+        ;;
+    esac
+  elif [[ x"$conf" == x"2m-2s-async" ]]; then
+    case $idx in
+      0)
+        echo "a"
+        ;;
+      1)
+        echo "a-s"
+        ;;
+      2)
+        echo "b"
+        ;;
+      3)
+        echo "b-s"
+        ;;
+      *)
+        ;;
+    esac
+  elif [[ x"$conf" == x"2m-2s-sync" ]]; then
+    case $idx in
+      0)
+        echo "a"
+        ;;
+      1)
+        echo "a-s"
+        ;;
+      2)
+        echo "b"
+        ;;
+      3)
+        echo "b-s"
+        ;;
+      *)
+        ;;
+    esac
+  fi
+
+}
+
+if [[ x"${NAME_SERVER}" == x"true" ]]; then
   launch_name_server
   exit 0
+fi
+
+if [[ x"${BROKER_SERVER}" == x"true" ]]; then
+  if [[ x"${BROKER_CLUSTER_CONF}" == x"" ]]; then
+    echo "launch broker as single"
+    launch_broker_cluster_server "./conf/broker.conf"
+  else
+    if [[ x"${BROKER_CLUSTER_IDX}" == x"" ]]; then
+      echo "invalid BROKER_CLUSTER_IDX"
+      exit 1
+    fi
+    echo "launch broker as cluster"
+    BROKER_CLUSTER_ID=$(get_broker_cluster_id $BROKER_CLUSTER_CONF $BROKER_CLUSTER_IDX) 
+    launch_broker_cluster_server "./conf/${BROKER_CLUSTER_CONF}/broker-${BROKER_CLUSTER_ID}.properties"
+  fi
 fi
 
